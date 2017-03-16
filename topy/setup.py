cd031505155fa9ec85d6bf2115bc80f583230aeb
Ivan Sosnovik <sosnovikivan@gmail.com>
2017-03-17 00:07:44 +0300

    Update setup.py

--- topy/setup.py
+++ topy/setup.py
@@ -3,7 +3,7 @@
 from distutils.core import setup
 
 setup(name='ToPy',
-      version='0.2.3',
+      version='0.2.4',
       description='Topology optimization with Python',
       author='William Hunter',
       url='https://github.com/williamhunter/topy',
