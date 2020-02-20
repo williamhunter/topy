89dcd1ca5df1a8fddf0f9fef395d6a8386223a09
Mohamed Laradji <malaradji@gmail.com>
2020-02-19 16:55:07 -0800

    pip: change version number to 0.4.0

--- topy/setup.py
+++ topy/setup.py
@@ -1,12 +1,17 @@
 ﻿#!/usr/bin/env python
+"""
+ToPy install script.
 
+Install ToPy through `python setup.py install`.
+"""
 from distutils.core import setup
 
-setup(name='ToPy',
-      version='0.3.2',
-      description='Topology optimization with Python',
-      author='William Hunter',
-      url='https://github.com/williamhunter/topy',
-      packages=['topy', 'topy.data'],
-      package_dir={'topy': ''}
-     )
+setup(
+    name="ToPy",
+    version="0.4.0",
+    description="Topology optimization with Python",
+    author="William Hunter",
+    url="https://github.com/williamhunter/topy",
+    packages=["topy", "topy.data"],
+    package_dir={"topy": ""},
+)
