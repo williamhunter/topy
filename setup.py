e6c6f9865efd4fdecc00f30ad6198fadf794d93a
Mohamed Laradji <malaradji@gmail.com>
2020-02-23 09:30:52 -0800

    refactor(setup): use a metadata.json file

--- setup.py
+++ setup.py
@@ -5,31 +5,20 @@ ToPy install script.
 Install ToPy through `python setup.py install`.
 """
 
-import sys
+import json
 import setuptools
 
-# Allow input of version from commandline.
-for i, arg in enumerate(sys.argv):
-    print(arg)
-    if arg.startswith("--version="):
-        version = sys.argv.pop(i).split("=")[1]
-        break
-else:
-    version = "0.4.0"
+# Get metadata.
+with open("metadata.json", "r") as f:
+    metadata = json.load(f)
 
-
+# Get project description.
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="topy-mlaradji",
-    version=version,
-    author="William Hunter",
-    author_email="williamhunter@users.noreply.github.com",
-    description="Topology optimization with Python",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/williamhunter/topy",
     packages=["topy", "topy.data"],
     install_requires=['typing', 'pathlib', 'matplotlib', 'sympy', 'numpy<=1.14', 'pyvtk', 'pysparse'],
     classifiers=[
@@ -38,4 +27,5 @@ setuptools.setup(
         "Operating System :: OS Independent",
     ],
     python_requires='~=2.7',
+    **metadata
 )
