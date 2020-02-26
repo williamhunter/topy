4aad570c57d6f02cbb0b20b6df39564428a569ee
Mohamed Laradji <malaradji@gmail.com>
2020-02-25 22:18:37 -0800

    feat(benchmark): add benchmark github action

--- tests/test_examples.py
+++ tests/test_examples.py
@@ -1,26 +1,26 @@
 #!/usr/bin/env python
-
+"""Test and benchmark examples."""
 # Author: William Hunter, Mohamed Laradji
 # This script handles both number of iterations and change stop criteria runs
 
 # Import required modules:
 from __future__ import print_function
 from pathlib import Path
+
 import pytest
 
 import topy
 
-@pytest.mark.parametrize("fname", [
-    str(filename) for filename in Path("examples").rglob("*.tpd")
-])
-def test_optimise(fname):
+
+@pytest.mark.parametrize(
+    "filename", (str(filename) for filename in Path("examples").rglob("*.tpd"))
+)
+def test_optimise(filename, benchmark):
     # type: (str) -> None
-    """Optimise the file at `fname`."""
-    print("Optimizing file '%s'..." % fname)
+    """Optimise the file at `filename`."""
+    print("Optimizing file '%s'..." % filename)
     # Set up ToPy:
     t = topy.Topology()
-    t.load_tpd_file(fname)
+    t.load_tpd_file(filename)
     t.set_top_params()
-    topy.optimise(t)
-    assert True
-    print("Successfully optimized file '%s'." % fname)
+    benchmark(topy.optimise, t)
